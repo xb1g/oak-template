1d60be832d7984a7fb88ecf987badbdd93d3f0a8
Bunyasit Fang <wachaa1319@gmail.com>
2022-12-29 15:06:59 +0700

    change to mongo driver

--- main.ts
+++ main.ts
@@ -3,37 +3,10 @@ import "https://deno.land/std@0.127.0/dotenv/load.ts";
 
 // import { User } from "@/models/user.ts";
 import { usersRouter } from "@/routes/user.route.ts";
-import { MongoClient } from "mongo";
-// import { Friend } from "./models/friend.ts";
-
-const envVars = Deno.env.toObject();
-const client = new MongoClient();
-
-// await client.connect(envVars.DB_URI);
-await client.connect({
-  db: envVars.DB_NAME,
-  tls: true,
-  servers: JSON.parse(envVars.DB_SERVERS).map((uri: string) => {
-    return {
-      host: uri,
-      port: 27017,
-    };
-  }),
-
-  credential: {
-    username: envVars.DB_USERNAME,
-    password: envVars.DB_PASSWORD,
-    db: envVars.DB_NAME,
-    mechanism: "SCRAM-SHA-1",
-  },
-});
-
-// const dbUris = JSON.parse(envVars.DB_SERVERS);
-
 const app = new Application();
 const router = new Router();
 
-// router.use("/users", usersRouter.routes());
+router.use("/users", usersRouter.routes());
 
 router.get("/", (ctx) => {
   ctx.response.body = "Hello World From Deno Edge";
